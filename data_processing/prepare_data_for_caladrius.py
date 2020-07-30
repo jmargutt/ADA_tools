71b98726ccbb959a2bff9315f2dcb7fd2e6a911c
jmargutti <jacopo.margutti@gmail.com>
2020-07-30 17:57:37 +0200

    u

--- data_processing/prepare_data_for_caladrius.py
+++ data_processing/prepare_data_for_caladrius.py
@@ -116,22 +116,22 @@ def get_image_list(root_folder):
 
 def save_image(image, transform, out_meta, image_path):
     # t = time.time()
-    out_meta.update(
-        {
-            "driver": "PNG",
-            "height": image.shape[1],
-            "width": image.shape[2],
-            "transform": transform,
-        }
-    )
-    with rasterio.open(image_path, "w", **out_meta) as dest:
-        dest.write(image)
+    # out_meta.update(
+    #     {
+    #         "driver": "PNG",
+    #         "height": image.shape[1],
+    #         "width": image.shape[2],
+    #         "transform": transform,
+    #     }
+    # )
+    # with rasterio.open(image_path, "w", **out_meta) as dest:
+    #     dest.write(image)
     # print('save rasterio', time.time() - t)
     # t = time.time()
-    # image = np.swapaxes(image, 0, 2)
-    # image = np.swapaxes(image, 0, 1)
-    # im = Image.fromarray(image)
-    # im.save(image_path)
+    image = np.swapaxes(image, 0, 2)
+    image = np.swapaxes(image, 0, 1)
+    im = Image.fromarray(image)
+    im.save(image_path)
     # print('save PIL', time.time() - t)
     return image_path
 
